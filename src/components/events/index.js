539bf3ef47516172c839536d2fbe253afb5a1b27
Kdc693 <122105152+Kdc693@users.noreply.github.com>
2023-06-16 12:14:43 +0700

    might need to adjust sizes

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,9 +1,41 @@
-import "./events.css";
+import "../../index.css"
+import day1 from "./images/day1.svg"
+import day2 from "./images/day2.svg"
+import day3 from "./images/day3.svg"
+
 const Events = () => {
   return (
-    <div className="events">
+    <div class="bg-gradient-to-b from-[#3F2E7C] to-[#7E4081]">
       <div className="navigationContainer">
-        <p>Events</p>
+
+        <div class="mx-auto pt-10 flex-none md:pt-[8%]">
+            <p style={{fontFamily: "InsideOut"}} class="text-3xl md:text-5xl text-white text-center">Events</p>
+        </div>
+
+        <div class="py-[20px] md:py-10 flex-none relative">
+            <p style={{fontFamily: "InsideOut"}} class="text-lg md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 1 - 6 Aug 2023</p>
+            <img src={day1} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
+            <div class="absolute left-[43%] md:left-[44%] top-[45%] md:top-[35%] md:mr-[10%] flex mr-[7%]">
+              <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-white md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
+            </div>
+        </div>
+
+        <div class="py-[20px] md:py-10 flex-none relative">
+            <p style={{fontFamily: "InsideOut"}} class="text-lg md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 2 - 7 Aug 2023</p>
+            <img src={day2} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
+            <div class="absolute left-[43%] md:left-[44%] top-[45%] md:top-[35%] md:mr-[10%] flex mr-[7%]">
+              <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-[#2E5E50] md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
+            </div>
+        </div>
+
+        <div class="py-[20px] md:py-10 flex-none relative">
+            <p style={{fontFamily: "InsideOut"}} class="text-lg md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 3 - 8 Aug 2023</p>
+            <img src={day3} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
+            <div class="absolute left-[43%] md:left-[44%] top-[45%] md:top-[35%] md:mr-[10%] flex mr-[7%]">
+              <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-[#484F2D] md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
+            </div>
+        </div>
+
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xxs: ['0.5rem', {
+        lineHeight: '0.5rem',
+      }],
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': ['1.5rem', {
+        lineHeight: '2rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '3xl': ['1.75rem', {
+        lineHeight: '2.15rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '400',
+      }],
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    screens: {
+      'sm': '0px',
+      // => @media (min-width: 640px) { ... }
+      
+      'md': '600px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 };
