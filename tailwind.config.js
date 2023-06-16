ce1a57e6e18e6723686dd39b1d599db0f325965f
Kdc693 <122105152+Kdc693@users.noreply.github.com>
2023-06-16 17:53:01 +0700

    added breakpoints, adjusted font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,22 @@ module.exports = {
       xxs: ['0.5rem', {
         lineHeight: '0.5rem',
       }],
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      '2xl': ['1.5rem', {
+      sm: ['0.8rem', {
+        lineHeight: '1rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      base: ['1rem', {
+        lineHeight: '1.25rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      xl: ['1.25rem', {
+        lineHeight: '1.5rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '500',
+      }],
+      '2xl': ['1.4rem', {
         lineHeight: '2rem',
         letterSpacing: '-0.01em',
         fontWeight: '500',
@@ -26,7 +38,11 @@ module.exports = {
       'sm': '0px',
       // => @media (min-width: 640px) { ... }
       
-      'md': '600px',
+      'ssmed': '520px',
+
+      'smed': '700px',
+
+      'md': '840px',
       // => @media (min-width: 1024px) { ... }
 
       'lg': '1024px',
