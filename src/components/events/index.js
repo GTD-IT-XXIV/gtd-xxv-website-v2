5ad314a2167a801be7d63e31a320a200c8df70a4
Kdc693 <122105152+Kdc693@users.noreply.github.com>
2023-06-25 21:05:01 +0700

    Update index.js

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -41,7 +41,7 @@ const Events = () => {
             <div class="py-[20px] md:py-10 flex-none relative">
                 <p style={{fontFamily: "InsideOut"}} class="text-lg smed:text-3xl md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 1 - 6 Aug 2023</p>
                 <img src={day1} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
-                <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[31%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
+                <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[35%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
                   <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-white ssmed:text-sm smed:text-base  md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
                 </div>
             </div>
@@ -51,7 +51,7 @@ const Events = () => {
           <div class="py-[20px] md:py-10 flex-none relative">
               <p style={{fontFamily: "InsideOut"}} class="text-lg smed:text-3xl md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 2 - 7 Aug 2023</p>
               <img src={day2} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
-              <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[31%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
+              <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[35%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
                 <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-[#2E5E50] ssmed:text-sm smed:text-base  md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
               </div>
           </div>
@@ -61,7 +61,7 @@ const Events = () => {
           <div class="py-[20px] md:py-10 flex-none relative">
               <p style={{fontFamily: "InsideOut"}} class="text-lg smed:text-3xl md:text-4xl text-white text-left pl-[5%] md:pl-[7%]">Day 3 - 8 Aug 2023</p>
               <img src={day3} class="h-full w-[90%] md:scale-[95%] md:py-5 mx-auto"></img>
-              <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[31%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
+              <div class="absolute left-[43%] top-[42%] ssmed:top-[35%] smed:top-[33%] md:top-[35%] lg:left-[44%] lg:top-[30%] md:mr-[10%] flex mr-[7%]">
                 <p style={{fontFamily: "Lato-Regular"}} class="text-xxs text-[#484F2D] ssmed:text-sm smed:text-base  md:text-xl lg:text-2xl xl:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet a lacus. Vestibulum dapibus nisi sit amet mattis dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et magna in orci placerat aliquam sit amet</p>
               </div>
           </div>
