3fe004a85343fc744a887fbaa50c48a93f18b696
JJAMES001@e.ntu.edu.sg <joshjms1607@gmail.com>
2023-07-01 18:46:03 +0800

    Add registration form and minor fixes

--- src/components/faq/contact.js
+++ src/components/faq/contact.js
@@ -1,15 +1,20 @@
+import Telegram from "./images/telegram.png";
+
 export default function Contact() {
     return (
-        <div className="rounded-2xl shadow m-3 h-[667px] bg-white">
+        <div className="rounded-2xl shadow m-3 bg-white">
             <div
-                className={`cursor-pointer accordion-header p-3 flex justify-between items-center rounded-t-xl bg-gray-300 h-[60px]`}
+                className={`cursor-pointer accordion-header p-3 flex justify-between items-center rounded-t-xl bg-gray-300`}
             >
                 <p className="m-0 font-bold text-xl">Contact Us</p>
             </div>
             <div
-                className={`flex h-[607px] accordion-content rounded-b-xl items-center justify-center`}
+                className={`p-3`}
             >
-                <div className="flex h-[540px] w-full bg-gray-300 rounded m-4" />
+                <a className="flex gap-3 items-center" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D">
+                    <img src={Telegram} className="w-8 h-8" />
+                    Telegram
+                </a>
             </div>
         </div>
     );
--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,27 +1,34 @@
 import AccordionGroup from "./accordion-group";
 import Contact from "./contact";
 import "./faq.css";
+import RegistrationForm from "./registration-form";
+
 const FAQ = () => {
     return (
         <div className="faq bg-gradient-to-b from-[#190F3E] to-[#7E4081]">
             <div className="navigationContainer">
-                <h1 className="text-center text-white mb-10">FAQ</h1>
+                <h1 className="text-center text-white my-10">FAQ</h1>
 
                 <div className="md:hidden w-[90%] mx-auto">
                     <div className="w-full">
-                        <Contact />
+                        <RegistrationForm />
                     </div>
-                    
+
                     <div className="w-full">
                         <AccordionGroup />
+                    </div>
+
+                    <div className="w-full">
+                        <Contact />
                     </div>
                 </div>
 
                 <div className="hidden md:flex w-[90%] mx-auto">
                     <div className="w-[50%]">
+                        <RegistrationForm />
                         <Contact />
                     </div>
-                    
+
                     <div className="w-[50%]">
                         <AccordionGroup />
                     </div>
