eeddc8726000ca9e7cb7b2d4a41835e8f479cf40
Priscilla Celine Setiawan <120435531+priscilla-celine@users.noreply.github.com>
2023-07-03 23:06:45 +0800

    <Update> content

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -4,38 +4,42 @@ import "./faq.css";
 import RegistrationForm from "./registration-form";
 
 const FAQ = () => {
-    return (
-        <div className="faq bg-gradient-to-b from-[#190F3E] to-[#7E4081]">
-            <div className="navigationContainer">
-                <h1 className="text-center text-white my-10">FAQ</h1>
+  return (
+    <div className="faq bg-gradient-to-b from-[#190F3E] to-[#7E4081]">
+      <div className="navigationContainer">
+        {/* <h1 className="text-center text-white my-10">FAQ & Registration</h1> */}
 
-                <div className="md:hidden w-[90%] mx-auto">
-                    <div className="w-full">
-                        <RegistrationForm />
-                    </div>
+        <div className="md:hidden w-[90%] mx-auto">
+          <h1 className="text-center text-white my-10">Registration</h1>
+          <div className="w-full">
+            <RegistrationForm />
+          </div>
 
-                    <div className="w-full">
-                        <AccordionGroup />
-                    </div>
+          <div className="w-full">
+            <h1 className="text-center text-white my-10">FAQ</h1>
+            <AccordionGroup />
+          </div>
 
-                    <div className="w-full">
-                        <Contact />
-                    </div>
-                </div>
-
-                <div className="hidden md:flex w-[90%] mx-auto">
-                    <div className="w-[50%]">
-                        <RegistrationForm />
-                        <Contact />
-                    </div>
-
-                    <div className="w-[50%]">
-                        <AccordionGroup />
-                    </div>
-                </div>
-            </div>
+          <div className="w-full">
+            <Contact />
+          </div>
         </div>
-    );
+
+        <div className="hidden md:flex w-[90%] mx-auto">
+          <div className="w-[50%]">
+            <h1 className="text-center text-white my-10">Registration</h1>
+            <RegistrationForm />
+            <Contact />
+          </div>
+
+          <div className="w-[50%]">
+            <h1 className="text-center text-white my-10">FAQ</h1>
+            <AccordionGroup />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FAQ;
--- src/components/faq/registration-form.js
+++ src/components/faq/registration-form.js
@@ -1,25 +1,25 @@
 export default function RegistrationForm() {
-    return (
-        <div className="rounded-2xl shadow m-3 bg-white">
-            <div
-                className={`cursor-pointer accordion-header p-3 flex justify-between items-center rounded-t-xl bg-gray-300`}
-            >
-                <p className="m-0 font-bold text-xl">Registration Form</p>
-            </div>
-            <div
-                className={`flex accordion-content rounded-b-xl items-center justify-center`}
-            >
-                <iframe
-                    src="https://docs.google.com/forms/d/e/1FAIpQLSdBv4c4LWXIJ-i0vxQypB61mO8lesnEEZaarXlelJyCsNIZ9w/viewform?embedded=true"
-                    width="100%"
-                    height="500px"
-                    frameborder="0"
-                    marginheight="0"
-                    marginwidth="0"
-                >
-                    Loading…
-                </iframe>
-            </div>
-        </div>
-    );
+  return (
+    <div className="rounded-2xl shadow m-3 bg-white">
+      <div
+        className={`cursor-pointer accordion-header p-3 flex justify-between items-center rounded-t-xl bg-gray-300`}
+      >
+        <p className="m-0 font-bold text-xl">Registration Form</p>
+      </div>
+      <div
+        className={`flex accordion-content rounded-b-xl items-center justify-center`}
+      >
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeg_PHaYJe9WdG16MbTpLY3QUDvGE5bVTyiAc8Fq3Vzlej0Eg/viewform"
+          width="100%"
+          height="500px"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
 }
