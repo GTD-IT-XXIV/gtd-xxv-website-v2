3e54e44769fb8ce074a4f3a147570b268315953f
jonathan-kelvin <jonathankelvinsantoso@gmail.com>
2023-07-21 17:06:48 +0800

    [FIX] Leaderboard

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -1,77 +1,67 @@
-import React, { useState } from "react";
-import leaderBoardText from "./leaderboard_text.png";
-import leftStar from "./star_kiri.png";
-import midStar from "./star_tengah.png";
-import rightStar from "./star_kanan.png";
-import redLine from "./garisnya.png";
+import React, { useState } from 'react';
+import leaderBoardText from './leaderboard_text.png';
+import leftStar from './star_kiri.png';
+import midStar from './star_tengah.png';
+import rightStar from './star_kanan.png';
+import redLine from './garisnya.png';
 
-const LeaderboardTable = ({ data }) => {
-  const [selectedDay, setSelectedDay] = useState("Overall");
-  const [filterType, setFilterType] = useState("House");
-
-  // GET HOUSE DATA ONLY
-  const houseData = [];
-
-  // NEW SET FOR HOUSES' POINTS
-  const houseSet = new Set();
-  data.forEach((e) => {
-    houseSet.add(e.house);
-  });
-
-  [...houseSet].forEach((house) => {
-    const currentHouseData = data.filter((e) => {
-      return house === e.house;
-    });
-
-    const newHouseData = {
-      house,
-      day1Point: 0,
-      day2Point: 0,
-      day3Point: 0,
-      totalPoint: 0,
-    };
-
-    currentHouseData.forEach((e) => {
-      newHouseData.day1Point += e.day1Point;
-      newHouseData.day2Point += e.day2Point;
-      newHouseData.day3Point += e.day3Point;
-    });
-
-    houseData.push(newHouseData);
-  });
-
-  // HOUSEDATA AS ORIGINAL STATE
+const LeaderboardTable = ({ ogData, houseData }) => {
+  const [selectedDay, setSelectedDay] = useState('Overall');
+  const [filterType, setFilterType] = useState('House');
   const [displayedData, setDisplayedData] = useState(houseData);
 
-  // FILTERTYPE CHANGE HOUSE/OG
   const handleFilterChange = ({ target: { value } }) => {
     setFilterType(value);
-    if (value === "House") {
+    setSelectedDay('Overall');
+    if (value === 'House') {
       setDisplayedData(houseData);
-    } else if (value === "OG") {
-      setDisplayedData(data);
+    } else if (value === 'OG') {
+      setDisplayedData(ogData);
     }
   };
 
-  // SORT THE DATA DISPLAYED
   displayedData.sort((a, b) => {
     let sortA;
     let sortB;
-    if (selectedDay === "Overall") {
-      sortA = a.day1Point + a.day2Point + a.day3Point;
-      sortB = b.day1Point + b.day2Point + b.day3Point;
-    } else if (selectedDay === "Day 1") {
+    if (selectedDay === 'Overall') {
+      sortA = a.totalPoints;
+      sortB = b.totalPoints;
+    } else if (selectedDay === 'Day 1') {
       sortA = a.day1Point;
       sortB = b.day1Point;
-    } else if (selectedDay === "Day 2") {
+    } else if (selectedDay === 'Day 2') {
       sortA = a.day2Point;
       sortB = b.day2Point;
-    } else if (selectedDay === "Day 3") {
+    } else if (selectedDay === 'Day 3') {
       sortA = a.day3Point;
       sortB = b.day3Point;
     }
     return sortB - sortA;
   });
+
+  const handleSort = day => {
+    setDisplayedData(data => {
+      const sortedData = [...data].sort((a, b) => {
+        let sortA;
+        let sortB;
+        if (day === 'Overall') {
+          sortA = a.totalPoints;
+          sortB = b.totalPoints;
+        } else if (day === 'Day 1') {
+          sortA = a.day1Points;
+          sortB = b.day1Points;
+        } else if (day === 'Day 2') {
+          sortA = a.day2Points;
+          sortB = b.day2Points;
+        } else if (day === 'Day 3') {
+          sortA = a.day3Points;
+          sortB = b.day3Points;
+        }
+        return sortB - sortA;
+      });
+      return sortedData;
+    });
+  };
 
   return (
     <div className="container-full">
@@ -82,11 +72,6 @@ const LeaderboardTable = ({ data }) => {
           alt="Leaderboard"
         />
         <div className="w-auto flex justify-center">
-          {/* <img
-            className="w-full object-scale-down h-14 sm:h-16 md:h-20 lg:h-24 mt-[24px] absolute right-[114px] top-32"
-            src={leftStar}
-            alt="star"
-          /> */}
           <img
             className="w-full object-scale-down h-14 sm:h-16 md:h-20 lg:h-24 mt-[24px] absolute right-[114px] top-20 sm:top-32"
             src={leftStar}
@@ -127,88 +112,79 @@ const LeaderboardTable = ({ data }) => {
         <div className="buttons-container flex justify-center relative bottom-[90px] top-20">
           <button
             className="bg-[#FFF2D0] border-2 border-[#C13E3E] ring ring-[#FEE794] ring-[2px] mx-1 sm:mx-2 px-4 sm:px-6 py-1 sm:py-2 text-[#C13E3E] text-[10px] sm:text-[14px] md:text-[20px] lg:text-[40px] hover:drop-shadow-lg rounded-[10px] sm:rounded-[20px]"
-            onClick={() => setSelectedDay("Overall")}
+            onClick={() => {
+              setSelectedDay('Overall');
+              handleSort('Overall');
+            }}
           >
             Overall
           </button>
           <button
             className="bg-[#FFF2D0] border-2 border-[#C13E3E] ring ring-[#FEE794] ring-[2px] mx-1 sm:mx-2 px-4 sm:px-6 py-1 sm:py-2 text-[#C13E3E] text-[10px] sm:text-[14px] md:text-[20px] lg:text-[40px] hover:drop-shadow-lg rounded-[10px] sm:rounded-[20px]"
-            onClick={() => setSelectedDay("Day 1")}
+            onClick={() => {
+              setSelectedDay('Day 1');
+              handleSort('Day 1');
+            }}
           >
             Day 1
           </button>
           <button
             className="bg-[#FFF2D0] border-2 border-[#C13E3E] ring ring-[#FEE794] ring-[2px] mx-1 sm:mx-2 px-4 sm:px-6 py-1 sm:py-2 text-[#C13E3E] text-[10px] sm:text-[14px] md:text-[20px] lg:text-[40px] hover:drop-shadow-lg rounded-[10px] sm:rounded-[20px]"
-            onClick={() => setSelectedDay("Day 2")}
+            onClick={() => {
+              setSelectedDay('Day 2');
+              handleSort('Day 2');
+            }}
           >
             Day 2
           </button>
           <button
             className="bg-[#FFF2D0] border-2 border-[#C13E3E] ring ring-[#FEE794] ring-[2px] mx-1 sm:mx-2 px-4 sm:px-6 py-1 sm:py-2 text-[#C13E3E] text-[10px] sm:text-[14px] md:text-[20px] lg:text-[40px] hover:drop-shadow-lg rounded-[10px] sm:rounded-[20px]"
-            onClick={() => setSelectedDay("Day 3")}
+            onClick={() => {
+              setSelectedDay('Day 3');
+              handleSort('Day 3');
+            }}
           >
             Day 3
           </button>
         </div>
 
         <div className="table-container flex items-center justify-center relative mt-40">
-          {/* border-table border-y-[30px] border-x-[20px] border-[#C13E3E] rounded-[30px] ring-[8px] ring-[#FEE794] bg-[#C13E3E] flex-center */}
           <div className="border-table border-y-[30px] border-x-[20px] border-[#C13E3E] rounded-[30px] ring-[8px] ring-[#FEE794] bg-[#C13E3E] flex-center">
             <table className="text-[#474747] text-center text-[16px] sm:text-[18px] md:text-[20px] lg:text-[40px] border-4 border-[#B17A7A] rounded-[10px] w-auto">
               <thead>
                 <tr className="border-b-2 border-[#745E5E]">
-                  <th className="bg-[#F4F4F4] font-bold px-8 py-2 text-[#C13E3E]">
-                    Rank
-                  </th>
-                  <th className="bg-[#F4F4F4] font-bold px-10 py-2 text-[#C13E3E]">
-                    {filterType}
-                  </th>
-                  <th className="bg-[#F4F4F4] font-bold px-3 py-2 text-[#C13E3E]">
-                    {
-                      selectedDay === "Overall"
-                        ? "Total Point"
-                        : "Total Point" /*: `Day ${selectedDay.slice(-1)} Point` */
-                    }
-                  </th>
+                  <th className="bg-[#F4F4F4] font-bold px-8 py-2 text-[#C13E3E]">Rank</th>
+                  <th className="bg-[#F4F4F4] font-bold px-10 py-2 text-[#C13E3E]">{filterType}</th>
+                  <th className="bg-[#F4F4F4] font-bold px-3 py-2 text-[#C13E3E]">{`${selectedDay} Points`}</th>
                 </tr>
               </thead>
               <tbody>
-                {/* [#F7D7C9] #red	
-									[#D9EDF6] #blue
-									[#E2EDBA] #green
-									[#F4DFF7] #purple */}
-
                 {displayedData.map((item, idx) => (
                   <tr
-                    key={`${item.day1Point} ${item.day2Point} ${item.day3Point} ${item.house}`}
                     className={`border-b-2 border-[#D2C2BB] ${
-                      ["Sadness", "Fear", "Disgust", "Anger"].includes(
-                        item.house
-                      )
-                        ? "house-row"
-                        : "og-row"
+                      ['Sadness', 'Fear', 'Disgust', 'Anger'].includes(item.house)
+                        ? 'house-row'
+                        : 'og-row'
                     }`}
                     style={{
                       backgroundColor:
-                        item.house === "Sadness"
-                          ? "#D9EDF6"
-                          : item.house === "Anger"
-                          ? "#F7D7C9"
-                          : item.house === "Disgust"
-                          ? "#E2EDBA"
-                          : item.house === "Fear"
-                          ? "#F4DFF7"
-                          : "",
+                        item.house === 'Sadness'
+                          ? '#D9EDF6'
+                          : item.house === 'Anger'
+                          ? '#F7D7C9'
+                          : item.house === 'Disgust'
+                          ? '#E2EDBA'
+                          : item.house === 'Fear'
+                          ? '#F4DFF7'
+                          : '',
                     }}
                   >
                     <td className="py-2">{idx + 1}</td>
+                    <td className="py-2">{filterType === 'House' ? item.house : item.group}</td>
                     <td className="py-2">
-                      {filterType === "House" ? item.house : item.group}
-                    </td>
-                    <td className="py-2">
-                      {selectedDay === "Overall"
-                        ? item.day1Point + item.day2Point + item.day3Point
-                        : item[`day${selectedDay.slice(-1)}Point`]}
+                      {selectedDay === 'Overall'
+                        ? item.totalPoints
+                        : item[`day${selectedDay.slice(-1)}Points`]}
                     </td>
                   </tr>
                 ))}
